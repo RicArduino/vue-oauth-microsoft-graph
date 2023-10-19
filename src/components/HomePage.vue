3bd7c6276f10dfa0bca7a760fd30c4a5175f583b
Ricard0 <116546461+RicArduino@users.noreply.github.com>
2023-10-19 17:15:22 +0200

    Ex 7: create BaseButton with primary color

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -3,11 +3,36 @@
       
       <div class="content">
         Here comes the content of the HomePage.
+        <button class="base-button" @mouseover="hover" @mouseleave="unhover" @focus="focus" @blur="unfocus" :disabled="false">
+        BaseButton with custom margin
+      </button>
+      <button class="base-button base-button--disabled" @mouseover="hover" @mouseleave="unhover" @focus="focus" @blur="unfocus" :disabled="true">
+        BaseButton disabled
+      </button>
       </div>
     </div>
   </template>
   
-  <style scoped>
+  <script>
+export default {
+  methods: {
+    hover() {
+      this.$el.querySelector('.base-button').style.backgroundColor = "#45a049";
+    },
+    unhover() {
+      this.$el.querySelector('.base-button').style.backgroundColor = "#4CAF50";
+    },
+    focus() {
+      this.$el.querySelector('.base-button').style.backgroundColor = "#45a049";
+    },
+    unfocus() {
+      this.$el.querySelector('.base-button').style.backgroundColor = "#4CAF50";
+    }
+  }
+}
+</script>
+
+<style scoped>
 .container {
   display: flex;          
   flex-direction: column; 
@@ -20,5 +45,24 @@
   padding: 20px;
   font-size: 18px;
 }
+
+.base-button {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 12px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  cursor: pointer;
+  border-radius: 4px;
+  margin-right: 10px;
+  transition: background-color 0.3s ease;
+}
+
+.base-button--disabled {
+  background-color: #ccc;
+  cursor: not-allowed;
+}
 </style>
-  
